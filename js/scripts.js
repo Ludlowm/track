c312a455edfa91423a55f1556f9255561e4a3d3d
Marko Ludlow <ludlow.marko@gmail.com>
2017-03-24 13:52:50 -0700

    fixed issues

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $("form#track-selector").submit(function(event) {
+    event.preventDefault();
+
+    var app = $("#app").val();
+    var website = $("#website").val();
+    var company = $("#company").val();
+    var design = $("#design").val();
+    var backEnd= $("#back-end").val();
+
+    if (website === "yes-website" || company === "yes-company") {
+      $("#result").text("You should choose the Ruby/Rails path.");
+
+    }else if (company === "yes-company" && backEnd === "yes-backend") {
+      $("#result").text("You should choose the PHP/Drupel path.");
+
+    }else if (app === "yes-app") {
+      $("#result").text("You should choose the Java/Javascript path.");
+
+    }else if (company === "yes-company") {
+      $("#result").text("You should choose the C# path due to the large backing of microsoft and wide acceptance in other corporations.")
+
+    }else if (design === "yes-design") {
+      $("#result").text("You should choose the CSS/Design path due to your creativity.")
+
+    } else {
+      $("#result").text("Maybe you should rethink.");
+    }
+  });
+});
